60d47fe666d7d474afc1e404a059d4af6e4c4b28
leecla <91344135+leecla@users.noreply.github.com>
2024-05-16 18:56:59 -0700

    Add files via upload

--- example_adc_dma_from_forum_copy_20240426204618.ino
+++ example_adc_dma_from_forum_copy_20240426204618.ino
@@ -1,10 +1,17 @@
 #include <driver/i2s.h>
 
 // I2S
-#define I2S_SAMPLE_RATE (60000)
+#define I2S_SAMPLE_RATE (100000)
 #define ADC_INPUT (ADC1_CHANNEL_4) //pin 32
 //#define ADC_INPUT (ADC1_CHANNEL_8) // pin 25
-#define I2S_DMA_BUF_LEN (8)
+#define I2S_DMA_BUF_LEN (1024)
+// signal generation stuff: 15KHz 50% duty cycle pwm wave
+#define PWM_OUT_SIGGEN_PIN (16)  // pin 16 (RX2) for pwm output to signal generator
+#define PWM_CHANNEL (0) // pwm properties
+#define PWM_RESOLUTION (8) // resolution (0 to 255)
+#define PWM_DUTY_CYCLE (60) // 50% duty cycle (127 is 1/2 of 255)
+#define PWM_FREQUENCY (15000) // 15KHz signal generated
+#define USER_OUT_PIN (17)
 
 // The 4 high bits are the channel, and the data is inverted
 size_t bytes_read;
@@ -52,9 +59,22 @@ void setup() {
   Serial.begin(115200);
 
   i2sInit();
+
+  // configure LED PWM functionalities
+  ledcSetup(PWM_CHANNEL, PWM_FREQUENCY, PWM_RESOLUTION);
+
+  // attach the channel to the GPIO pin to be controlled
+  ledcAttachPin(PWM_OUT_SIGGEN_PIN, PWM_CHANNEL);
+
+  // user output pin setup
+  pinMode(USER_OUT_PIN, OUTPUT);
 }
 
 void loop() {
+
+  digitalWrite(USER_OUT_PIN, LOW); // set user output low
+
+  ledcWrite(PWM_CHANNEL, PWM_DUTY_CYCLE); // start signal generation
 
   unsigned long startMicros = ESP.getCycleCount();
 
@@ -64,11 +84,17 @@ void loop() {
 
   loopTime = stopMicros - startMicros;
 
-  if (millis() - lastTimePrinted >= 100) {
+  if (millis() - lastTimePrinted >= 25) {
     Serial.println("------------------");
     Serial.println(buffer[0] & 0x0FFF);
     //example_disp_buf((uint8_t*) buffer, sizeof(buffer));
-    Serial.println(loopTime);
+    //Serial.println(loopTime);
     lastTimePrinted = millis();
+    if ((buffer[0] & 0x0FFF) >= 25){  // detect high amplitude
+      Serial.println("---YEA---");
+      digitalWrite(USER_OUT_PIN, HIGH);   // set user output high if high amplitude detected
+      delay(500);
+    }
+
   }
 }
